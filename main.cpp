2ae679077f5101d058fb32cadf45972fbec55ceb
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-10-25 12:54:31 +0300

    sum of square

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
+
 #include <iostream>
 
 int main()
 {
-  size_t count = 0;
-  int t = 0;
+  size_t t = 0;
+  double sum = 0;
   while(std::cin >> t){
-    count++;
+    sum += t*t;
   }
   if(std::cin.eof()){
-    std::cout << count << '\n';
+    std::cout << sum << '\n';
     return 0;
   }else if (std::cin.fail()) {
     std::cout << "Sudar, vse ploho\n";
     return 1;
   }
 }
+
