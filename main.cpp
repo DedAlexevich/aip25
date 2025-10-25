b1254c74082f87dec4acbd9e8ac28c6f576d51c8
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-10-25 12:38:43 +0300

    Get info, that task is gifferent

--- main.cpp
+++ main.cpp
@@ -2,23 +2,17 @@
 
 int main()
 {
-  double sum = 0;
   size_t count = 0;
   int t = 0;
   while(true){
     std::cin >> t;
     if(std::cin.eof()){
-      if(count == 0){
-        std::cout << 0 << '\n';
-        return 0;
-      }
-      std::cout << sum/count << '\n'; 
+      std::cout << count << '\n';
       return 0;
     }else if (std::cin.fail()) {
       std::cout << "Sudar, vse ploho\n";
       return 1;
     }
-    sum += t;
     count++;
   }
 
