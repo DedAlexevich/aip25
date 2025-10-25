272635495cd4be2b64d9319d8a3a153523eadf15
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-10-25 14:46:24 +0300

    Edit fun isPth, изменены типы данных с size_t на int, тк по тз не сказано знаковые или беззнаковые числа

--- main.cpp
+++ main.cpp
@@ -1,32 +1,49 @@
-
 #include <iostream>
 
-bool isPth(size_t a, size_t b, size_t c);
+bool isPth(int a, int b, int c);
+
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
 
 int main()
 {
-  size_t a = 0, b = 0, c = 0;
+  int a = 0, b = 0, c = 0;
   size_t count = 0;
   std::cin >> c >> b;
+    
+  if (b <= 0 || c <= 0){
+      std::cerr << "The side lengths are always positive\n";
+      return 1;
+  }
   while (std::cin >> a) {
     count += isPth(a,b,c);
     c = b;
     b = a;
   }
+    
   if (std::cin.eof()){
     std::cout << count << '\n';
     return 0;
-  }else if (std::cin.fail()) {
-    std::cout << "Man, u are mistake\n";
+  } else if (std::cin.fail()) {
+    std::cerr << "Сouldn't count the sequence\n";
     return 1;
   }
 }
 
 
-bool isPth(size_t a, size_t b, size_t c)
+bool isPth(int a, int b, int c)
 {
-  bool d = a*a == (b*b + c*c);
-  d = d || (b*b == (a*a + c*c));
-  d = d || (c*c == (a*a + b*b));
-  return d;
+  int greate = max(a,max(b,c));
+  int lower = min(a,min(b,c));
+  int median = a - greate + b - lower + c;
+  return greate * greate == (lower * lower + median * median);
 }
+
