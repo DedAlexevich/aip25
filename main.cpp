86593902e865e6da682fc624de2ca6840d664c82
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-10-25 12:50:12 +0300

    end of 1 task

--- main.cpp
+++ main.cpp
@@ -4,16 +4,14 @@ int main()
 {
   size_t count = 0;
   int t = 0;
-  while(true){
-    std::cin >> t;
-    if(std::cin.eof()){
-      std::cout << count << '\n';
-      return 0;
-    }else if (std::cin.fail()) {
-      std::cout << "Sudar, vse ploho\n";
-      return 1;
-    }
+  while(std::cin >> t){
     count++;
   }
-
+  if(std::cin.eof()){
+    std::cout << count << '\n';
+    return 0;
+  }else if (std::cin.fail()) {
+    std::cout << "Sudar, vse ploho\n";
+    return 1;
+  }
 }
