d18b8799343300a400540982e4c01e4986bb40ea
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-10-25 12:26:04 +0300

    Start of release

--- main.cpp
+++ main.cpp
@@ -1,2 +1,17 @@
+#include <iostream>
+
 int main()
-{}
+{
+  double sum = 0;
+  size_t count = 0;
+  int t = 0;
+  while(true){
+    std::cin >> t;
+    if(std::cin.eof){
+      if(count == 0) return 0; 
+      std::cout << sum/count << '\n'; 
+      return 0;
+    }
+    
+  }
+}
